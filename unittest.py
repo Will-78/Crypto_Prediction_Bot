73c2f1046f63a05373041ade0a6b757ec8d463ba
adamBataineh <138632403+adamBataineh@users.noreply.github.com>
2024-06-25 19:40:47 -0500

    Create unittest.py

---
+++ unittest.py
@@ -0,0 +1,24 @@
+'''This just serves as an outline and follows the general logic we can use
+when performing unit tests. Link on unittest.mock and how @patch works:
+https://docs.python.org/3/library/unittest.mock.html (may not be needed later)'''
+
+import unittest
+from unittest.mock import patch
+
+class TestCryptoTradingBot(unittest.TestCase):
+
+    @patch('your_module.fetch_reddit_posts')
+    def test_fetch_reddit_posts(self, mock_fetch):
+        mock_fetch.return_value = [{'id': 'test1', 'title': 'Bitcoin is doing great!'}]
+        result = fetch_reddit_posts('cryptocurrency')
+        self.assertEqual(len(result), 1)
+        self.assertEqual(result[0]['title'], 'Bitcoin is doing great!')
+
+    @patch('your_module.get_sentiment')
+    def test_get_sentiment(self, mock_sentiment):
+        mock_sentiment.return_value = 'Positive'
+        sentiment = get_sentiment('Bitcoin is doing great!')
+        self.assertEqual(sentiment, 'Positive')
+
+if __name__ == '__main__':
+    unittest.main()
